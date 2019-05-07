1253184478bc6d398ce320f1caef012307a48856
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-05-07 14:39:07 +0700

    Add files via upload

---
+++ Latihan Incheritance/Latihan Incheritance/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Incheritance
+{
+    public class student : Program
+    {
+        public string student { get; set; }
+        public 
+    }
+}
